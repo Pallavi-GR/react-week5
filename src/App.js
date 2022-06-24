1ff21f8d201e9b756ff7e6ea778d146cb06ec8f2
Pallavi G R <pallavi.gr3@gmail.com>
2022-06-24 14:09:49 +0200

    Basic structure setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Welcome to Coding</h1>
+        <p>React Week 5</p>
+      </header>
+    </div>
+  );
+}
