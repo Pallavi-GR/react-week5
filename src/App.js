395086d0288ba7f245a9faa06ebd2eff7f370baf
Pallavi G R <pallavi.gr3@gmail.com>
2022-07-10 10:58:13 +0200

    framework changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Welcome to Coding</h1>
-        <p>React Week 5</p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1> Welcome to Coding</h1>
+          <Weather />
+          <p>React Week 5</p>
+        </header>
+
+        <footer>
+          This page is code by Pallavi G R and is open sourdec on GITHUB{" "}
+          <a
+            href="https://github.com/Pallavi-GR/react-week5.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            @ PALLAVI GR
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./weather.css";
+
+export default function Weather() {
+  return (
+    <div classname="weather">
+      <p>Hello from Weather</p>
+    </div>
+  );
+}
